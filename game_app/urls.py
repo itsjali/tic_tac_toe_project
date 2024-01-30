b08ba79b70e1c824b290e6f98a405d1f61efb8bf
jali <josh.ali@zerodeposit.com>
2024-01-30 21:33:23 +0000

    Refine game over view

--- game_app/urls.py
+++ game_app/urls.py
@@ -3,6 +3,6 @@ from game_app.v2.views import game_play, game_over, game_home
 
 urlpatterns = [
     path("home/", game_home, name="game_home"),
-    path("play/<int:board_id>/<int:player_game_id>", game_play, name="game_play"),
-    path("game_over/<int:board_id>/", game_over, name="game_over"),
+    path("play/<int:board_id>/<int:player_game_id>/", game_play, name="game_play"),
+    path("game_over/<int:board_id>/<int:player_game_id>/", game_over, name="game_over"),
 ]
