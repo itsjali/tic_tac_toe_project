87c5953e5784058d36844a72d76784a26c0c5160
Joshua Ali <85988925+itsjali@users.noreply.github.com>
2024-02-23 10:53:05 +0000

    Create more functionality in the views and add property in the model (#4)
    * create base header and add it in all templates
    
    * add property in the model and create more validations in the views
    
    ---------
    
    Co-authored-by: jali <josh.ali@zerodeposit.com>

--- game_app/urls.py
+++ game_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from game_app.v2.views import game_play, game_over, game_home, signup_view, login_view
+from game_app.v2.views import new_game, active_games, game_play, game_over, game_home, signup_view, login_view
 
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path("logout/", login_view, name="logout"),
     path("signup/", signup_view, name="signup"),
     path("home/", game_home, name="game_home"),
-    path("play/<int:board_id>/<int:player_game_id>/", game_play, name="game_play"),
-    path("game_over/<int:board_id>/<int:player_game_id>/", game_over, name="game_over"),
+    path("new_game/<int:opponent_user_id>/", new_game, name="new_game"),
+    path("active_games/<int:game_id>/", active_games, name="active_games"),
+    path("play/<int:game_id>/<int:player_id>/", game_play, name="game_play"),
+    path("game_over/<int:game_id>/", game_over, name="game_over"),
 ]
